9a4b990db536f53c315f2e0efc3d8c51451431f7
Gabriel Steven Machicao Quispe <gmachicaoq@unsa.edu.pe>
2022-05-07 17:49:29 -0500

    Se decidio arreglar el titulo del Ejercicio_2 del html, luego se escribio todo en la funcion obtenerCodigo y este este sin parametros, además de arreglar el substring.

--- Ejercicio_4/js/script.js
+++ Ejercicio_4/js/script.js
@@ -1,6 +1,8 @@
-function obtenerCodigo(link){
-  let codigo = link.subString(25,36);
-  let letras = codigo.subString(0,3) + codigo.subString(5,8) + codigo.subString(10,12);
+function obtenerCodigo(){
+  let f = document.getElementById("codigo");
+  const link = f.value;
+  let codigo = link.substring(25,36);
+  let letras = codigo.substring(0,3) + codigo.substring(5,8) + codigo.substring(10,12);
   document.getElementById("newcodigo").innerHTML =  letras;
 }
 
