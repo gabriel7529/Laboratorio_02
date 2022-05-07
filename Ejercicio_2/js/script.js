3c16e53460e98eb14381af239f044a74b46d7079
Gabriel Steven Machicao Quispe <gmachicaoq@unsa.edu.pe>
2022-05-07 15:21:29 -0500

    Se puso un id en el html y se creo un archivo js, que contiene la función para cambiar la palabra.

---
+++ Ejercicio_2/js/script.js
@@ -0,0 +1,8 @@
+function convertirPalabra(){
+  let tagWord = document.getElementId("word");
+  let word = tagWord.value;
+  const newWord = "";
+  for (let i=word.length; i>-1;i--){
+    newWord = newWord.concat(word.substring(length-1;length));
+  }
+}
