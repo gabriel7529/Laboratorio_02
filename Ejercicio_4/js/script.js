348db0d9f9c56899b21666136fd3e1280d801b50
Gabriel Steven Machicao Quispe <gmachicaoq@unsa.edu.pe>
2022-05-07 17:52:24 -0500

    Se cambio los números de substring para que este ubique las partes del link correctamente

--- Ejercicio_4/js/script.js
+++ Ejercicio_4/js/script.js
@@ -1,8 +1,8 @@
 function obtenerCodigo(){
   let f = document.getElementById("codigo");
   const link = f.value;
-  let codigo = link.substring(25,36);
-  let letras = codigo.substring(0,3) + codigo.substring(5,8) + codigo.substring(10,12);
+  let codigo = link.substring(24,36);
+  let letras = codigo.substring(0,3) + codigo.substring(4,8) + codigo.substring(9,13);
   document.getElementById("newcodigo").innerHTML =  letras;
 }
 
