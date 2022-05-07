578267b8c1a670716404bc39edce91adde6555ae
Gabriel Steven Machicao Quispe <gmachicaoq@unsa.edu.pe>
2022-05-07 17:05:16 -0500

    Se creo una funcion diasRestantes, en la cual invoca el dia actual y luego el dia de arequipa, se restan dichos dias y luego se le divide en 1000*3600*24, y luego se le suma 1.

---
+++ Ejercicio_3/js/script.js
@@ -0,0 +1,7 @@
+function diasRestantes (){
+  let inicio = new Date();
+  let fin = new Date("2022,08,15");
+  let diferencia = Math.abs(fin - inicio);
+  let restantes = Math.round(diferencia / (1000*3600*24))+1;
+  document.getElementById("days").InnertHtml = restantes;
+}
