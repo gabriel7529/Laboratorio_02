3e7c13b5cf5dc66090189bdd6808a690cbdc6cd8
Gabriel Steven Machicao Quispe <gmachicaoq@unsa.edu.pe>
2022-05-07 16:03:56 -0500

    Se corrigio el cambio que no se hizo en el anterior commit de cambio de concant por +=

--- Ejercicio_2/js/script.js
+++ Ejercicio_2/js/script.js
@@ -1,8 +1,9 @@
 function convertirPalabra(){
-  let tagWord = document.getElementById("word");
+  const tagWord = document.getElementById("word");
   const word = tagWord.value ;
   let newWord = "";
   for (let i= word.length ; i>0;i--){
-    newWord.concat(word.substring(i-1,i));
+    newWord+=word.substring(i-1,i);
   }
+  document.getElementById("neWord").innerHTML = newWord;
 }
