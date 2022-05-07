0123d9aa025be8e11d5d8f0db4bf0e3c702767de
Gabriel Steven Machicao Quispe <gmachicaoq@unsa.edu.pe>
2022-05-07 17:10:49 -0500

    Se corregio el innerHTML, ademas de quitar las comillas y agregar en el innerHTML, un texto.

--- Ejercicio_3/js/script.js
+++ Ejercicio_3/js/script.js
@@ -3,5 +3,5 @@ function diasRestantes (){
   let fin = new Date("2022,08,15");
   let diferencia = Math.abs(fin - inicio);
   let restantes = Math.round(diferencia / (1000*3600*24))+1;
-  document.getElementById("days").InnertHtml = restantes;
+  document.getElementById("days").innerHTML = "Faltan " + restantes + " Días.";
 }
